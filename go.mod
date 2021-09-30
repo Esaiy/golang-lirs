c3726601033c7f2249705d449e6db39ec6c77afe
Esaiy <93sultanbaney@gmail.com>
2021-09-30 12:17:05 +0700

    update algo with ordered map

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/esaiy/golang-lirs
+
+go 1.16
+
+require (
+	github.com/Velocidex/ordereddict v0.0.0-20210502082334-cf5d9045c0d1
+	github.com/cevaris/ordered_map v0.0.0-20190319150403-3adeae072e73
+	github.com/secnot/orderedmap v0.0.0-20170705091748-a05363cca499
+)
--- main.go
+++ main.go
@@ -8,22 +8,27 @@ import (
 	"strconv"
 	"strings"
 	"time"
+
+	"github.com/secnot/orderedmap"
 )
 
 var (
-	stack, list []int
-	hit, miss       = 0, 0
-	cacheSize   int = 500000
-	LIRSize     int = cacheSize * 99 / 100
-	HIRSize     int = cacheSize / 100
-	LIR             = make(map[int]int)
-	HIR             = make(map[int]int)
+	// HIRSize      int = 1
+	hit, miss        = 0, 0
+	cacheSize    int = 500000
+	LIRSize      int = cacheSize * 99 / 100
+	HIRSize      int = cacheSize / 100
+	orderedStack     = orderedmap.NewOrderedMap()
+	orderedList      = orderedmap.NewOrderedMap()
+	LIR              = make(map[int]int)
+	HIR              = make(map[int]int)
 )
 
 func main() {
 	var (
 		start    = time.Now()
-		filePath = "./test/data/web1_input.txt"
+		filePath = "./data/web1_input.txt"
+		// filePath = "./data/test_input.txt"
 	)
 
 	fmt.Println(LIRSize, HIRSize)
@@ -46,49 +51,70 @@ func main() {
 
 	var duration = time.Since(start)
 
-	fmt.Printf("HIT : %v\nMISS : %v\nHIT RATE : %v\n", hit, miss, (hit+miss)/2)
+	fmt.Println("List size :", orderedList.Len())
+	fmt.Println("Stack size :", orderedStack.Len())
+	fmt.Printf("HIT : %v\nMISS : %v\nHIT RATE : %v\n", hit, miss, float32(hit)/float32((hit+miss)))
 	fmt.Printf("Duration : %v\n", duration.Seconds())
 }
 
 func lirs(line string) {
 	var block = (strings.Split(line, ","))[0]
+	// fmt.Printf("-------\nCurrent key : %v\n", block)
+	// printStack()
+	// printList()
+	// printCache()
 
 	var blockNum, err = strconv.Atoi(block)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	if len(stack) < LIRSize {
+	if len(LIR) < LIRSize {
 		miss += 1
+		// fmt.Println("---miss---")
 		addToStack(blockNum)
 		makeLIR(blockNum)
 		return
 	}
 
-	if value, _ := LIR[blockNum]; value == 1 {
+	if _, ok := LIR[blockNum]; ok {
 		hit += 1
+		// fmt.Println("---HIT---")
+		key, _, _ := orderedStack.GetFirst()
+		keyInInt, _ := strconv.Atoi(key.(string))
+		if keyInInt == blockNum {
+			stackPrunning(false)
+		}
 		addToStack(blockNum)
 		return
 	}
 
-	if value, _ := HIR[blockNum]; value == 0 {
-		miss += 1
-		removeListFront()
-
-		if _, found := isBlockExists(blockNum, stack); found {
+	if _, ok := orderedList.Get(strconv.Itoa(blockNum)); ok {
+		hit += 1
+		// fmt.Println("---hit---")
+		if _, ok := orderedStack.Get(strconv.Itoa(blockNum)); ok {
 			makeLIR(blockNum)
-			addToStack(blockNum)
-			stackPrunning()
+			removeFromList(blockNum)
+			stackPrunning(true)
 		} else {
 			addToList(blockNum)
 		}
+		addToStack(blockNum)
 		return
 	} else {
-		hit += 1
-		if _, found := isBlockExists(blockNum, stack); found {
+		miss += 1
+		// fmt.Println("---MISS---")
+		if orderedList.Len() == HIRSize {
+			key, _, _ := orderedList.PopLast()
+			keyInInt, _ := strconv.Atoi(key.(string))
+			removeFromList(keyInInt)
+		}
+
+		if _, ok := orderedStack.Get(strconv.Itoa(blockNum)); ok {
 			makeLIR(blockNum)
-			stackPrunning()
+			stackPrunning(true)
 		} else {
+			makeHIR(blockNum)
 			addToList(blockNum)
 		}
 		addToStack(blockNum)
@@ -97,72 +123,83 @@ func lirs(line string) {
 }
 
 func addToStack(block int) {
-	if pos, found := isBlockExists(block, stack); found {
-		RemoveIndex(stack, pos)
+	key := strconv.Itoa(block)
+	if _, ok := orderedStack.Get(key); ok {
+		orderedStack.MoveLast(key)
+		return
 	}
-
-	stack = append([]int{block}, stack...)
+	orderedStack.Set(key, 1)
 }
 
 func addToList(block int) {
-	if pos, found := isBlockExists(block, list); found {
-		RemoveIndex(list, pos)
+	key := strconv.Itoa(block)
+	if orderedList.Len() == HIRSize {
+		orderedList.PopLast()
 	}
-
-	list = append([]int{block}, list...)
+	orderedList.Set(key, 1)
+	orderedList.MoveFirst(key)
 }
 
-func isBlockExists(block int, slice []int) (int, bool) {
-	pos, found := Find(slice, block)
-	if found {
-		return pos, true
-	}
-
-	return -1, false
-}
-
-func Find(slice []int, val int) (int, bool) {
-	for i, item := range slice {
-		if item == val {
-			return i, true
-		}
-	}
-	return -1, false
-}
-
-func RemoveIndex(s []int, index int) []int {
-	ret := make([]int, 0)
-	ret = append(ret, s[:index]...)
-	return append(ret, s[index+1:]...)
+func removeFromList(block int) {
+	key := strconv.Itoa(block)
+	orderedList.Delete(key)
 }
 
 func makeLIR(block int) {
 	LIR[block] = 1
-	HIR[block] = 0
+	removeFromList(block)
+	delete(HIR, block)
 }
 
 func makeHIR(block int) {
 	HIR[block] = 1
-	LIR[block] = 0
+	delete(LIR, block)
 }
 
-func removeListFront() {
-	if len(list) == 0 {
-		return
-	}
-	list = list[1:]
-}
-
-func stackPrunning() {
-	var first bool = false
-	for i := len(stack) - 1; i >= 0; i-- {
-		if value, _ := LIR[stack[i]]; value == 1 {
-			if !first {
-				first = true
-				makeHIR(stack[i])
-			} else {
-				stack = stack[:i-1]
-			}
+func stackPrunning(removeLIR bool) {
+	key, _, _ := orderedStack.PopFirst()
+	keyInInt, _ := strconv.Atoi(key.(string))
+	if removeLIR {
+		makeHIR(keyInInt)
+		if orderedList.Len() == HIRSize {
+			orderedList.PopLast()
 		}
+		orderedList.Set(key, 1)
+		orderedList.MoveFirst(key)
 	}
+
+	iter := orderedStack.Iter()
+	for k, _, ok := iter.Next(); ok; k, _, ok = iter.Next() {
+		keyInInt, _ := strconv.Atoi(k.(string))
+		if _, ok := LIR[keyInInt]; ok {
+			break
+		}
+		orderedStack.PopFirst()
+	}
+}
+
+func printStack() {
+	iter := orderedStack.Iter()
+	fmt.Printf("Stack : ")
+	for k, _, ok := iter.Next(); ok; k, _, ok = iter.Next() {
+		fmt.Printf("%v ", k)
+	}
+	fmt.Println()
+}
+
+func printList() {
+	iter := orderedList.Iter()
+	fmt.Printf("List : ")
+	for k, _, ok := iter.Next(); ok; k, _, ok = iter.Next() {
+		fmt.Printf("%v ", k)
+	}
+	fmt.Println()
+}
+
+func printCache() {
+	fmt.Printf("Cache : ")
+	for key, _ := range cache {
+		fmt.Printf("%v ", key)
+	}
+	fmt.Println()
 }
